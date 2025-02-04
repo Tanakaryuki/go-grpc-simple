1d9c27d6f13901e0fd10f4c42d230c20efa643e1
Tanakaryuki <tnk3257.rt@gmail.com>
2025-02-04 13:10:10 +0900

    fix: url取り出し

--- k6/script.js
+++ k6/script.js
@@ -7,13 +7,14 @@ export let options = {
 };
 
 export default function () {
-    let res = http.get('http://localhost:8080/hello');
+    let url = 'http://127.0.0.1:59789';
+    let res = http.get(`${url}/hello`);
     check(res, {
         'status is 200': (r) => r.status === 200,
         'body is "Hello World\\n"': (r) => r.body === 'Hello World\n',
     });
 
-    res = http.get('http://localhost:8080/health');
+    res = http.get(`${url}/health`);
     check(res, {
         'status is 200': (r) => r.status === 200,
         'body is "hello\\n"': (r) => r.body === 'hello\n',
